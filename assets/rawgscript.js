6d5cd8d42e4836d07a0ad6d4f77fd3f489f8c46d
danocide <dan.c.reilly@gmail.com>
2023-01-12 14:47:58 -0500

    added for loop for genre name

--- assets/rawgscript.js
+++ assets/rawgscript.js
@@ -5,6 +5,7 @@ var genresContainer = document.getElementById('modalGenre')
 var nameContainer = document.getElementById('modalName')
 var gameCover = document.getElementById('modalCover')
 var genreUl = document.createElement('ul')
+      genreUl.classList.add('genre')
 var genreLi = document.createElement('li')
 
 function getApi(requestUrl) {
@@ -16,20 +17,25 @@ function getApi(requestUrl) {
     .then(function (data) {
       console.log(data);
 
-      for (var i = 0; i < data.results[0].length; i++)
-      nameContainer.textContent = data.results[0].name;
-      gameCover.src = data.results[0].background_image;
-      genreLi.textContent = data.results[0].genres[0].name;
-      genreUl.textContent = 'Genres:'
-  
-      genresContainer.appendChild(genreUl);
-      genreUl.appendChild(genreLi);
+      for (var i = 0; i < data.results[0].genres.length; i++) {
+        console.log(data.results[0].genres[i])
+        nameContainer.textContent = data.results[0].name;
+        gameCover.src = data.results[0].background_image;
 
+        console.log("here is the name", data.results[0].genres[i].name)
 
+        // document.getElementById('genre').textContent = (data.results[0].genres[i].name);
+        genreLi.textContent = data.results[0].genres[i].name;
+        genreUl.textContent = 'Genres:'
+
+        genresContainer.appendChild(genreUl);
+        genreUl.appendChild(genreLi);
+
+      }
     })
     .then(function (gameGenres) {
       console.log(gameGenres)
-    
+
     });
 }
 
