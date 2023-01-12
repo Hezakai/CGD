28724fdcd239bffd38f35ea855e598edcdbd42ae
danocide <dan.c.reilly@gmail.com>
2023-01-12 14:00:17 -0500

    adding ul and li to genre to list all items

--- assets/rawgscript.js
+++ assets/rawgscript.js
@@ -4,31 +4,33 @@ var responseText = document.getElementById('response-text');
 var genresContainer = document.getElementById('modalGenre')
 var nameContainer = document.getElementById('modalName')
 var gameCover = document.getElementById('modalCover')
+var genreUl = document.createElement('ul')
+var genreLi = document.createElement('li')
 
 function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
       console.log(response);
       return response.json();
-  })
-    .then(function(data) {
+    })
+    .then(function (data) {
       console.log(data);
-      // console.log(data.results[0].genres);
+
+      for (var i = 0; i < data.results[0].length; i++)
       nameContainer.textContent = data.results[0].name;
       gameCover.src = data.results[0].background_image;
+      genreLi.textContent = data.results[0].genres[0].name;
+      genreUl.textContent = 'Genres:'
+  
+      genresContainer.appendChild(genreUl);
+      genreUl.appendChild(genreLi);
 
-      for (var i = 0; i < data.results.length; i++) {
-        console.log(data.results[i])
-  
-      }
-      
-      for (var i = 0; i < data.results.length; i++) {
-        console.log(data.results[0].genres[i])
-        genresContainer.textContent = 'Genre: ' + data.results[0].genres[0].name;
+
+    })
+    .then(function (gameGenres) {
+      console.log(gameGenres)
     
-      }
-  });
-  
+    });
 }
 
 getApi(requestUrl);
