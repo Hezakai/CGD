30ab17b128e0cd8e794c94d13385208ce10d4515
danocide <dan.c.reilly@gmail.com>
2023-01-17 13:17:00 -0500

    search function added with rawg API

--- assets/rawgscript.js
+++ assets/rawgscript.js
@@ -1,4 +1,5 @@
-var requestUrl = 'https://api.rawg.io/api/games?key=09641c2e0fcd40ee8c29ef6543fe1103&per_page=5&dates=2019-09-01,2019-09-30&platforms=4&stores=1';
+var gameSlug = ""
+
 
 var responseText = document.getElementById('response-text');
 var genresContainer = document.getElementById('modalGenre')
@@ -9,37 +10,32 @@ var genreUl = document.createElement('ul')
 var genreLi = document.createElement('li')
 
 
+
 function getApi(requestUrl) {
   fetch(requestUrl)
+  
     .then(function (response) {
       console.log(response);
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      gameCover.src = data.results[0].background_image;
-
-      for (var i = 0; i < data.results[0].genres.length; i++) {
-        console.log(data.results[0].genres[i])
-        nameContainer.textContent = data.results[0].name;
-        var genreArr = [data.results[0].genres[0].name, data.results[0].genres[1].name, data.results[0].genres[2].name, data.results[0].genres[3].name]
-
-        // console.log("here is the name", data.results[0].genres[i].name)
-
-        // document.getElementById('genre').textContent = (data.results[0].genres[i].name);
-        genreLi.textContent = genreArr;
-        genreUl.textContent = 'Genres:'
-
-        genreUl.appendChild(genreLi);
-        genresContainer.appendChild(genreUl);
-        
-
-      }
-    })
-    .then(function (gameGenres) {
-      console.log(gameGenres)
+     
 
     });
 }
 
+var btnResponse = ""
+
+function initSearch() {
+
+let input = document.getElementById("searchInput").value;
+btnResponse = input;
+gameSlug = btnResponse.replace(/\s+/g, '-').toLowerCase();
+var requestUrl = 'https://api.rawg.io/api/games/' + gameSlug + '?key=09641c2e0fcd40ee8c29ef6543fe1103';
+console.log(gameSlug)
 getApi(requestUrl);
+console.log(requestUrl)
+}
+
+
